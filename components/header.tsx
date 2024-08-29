101144df1e6abf105a99496c2129424dfd9f5bb1
Sander Nicolaysen <sander.nicolaysen@tietoevry.com>
2024-08-29 09:47:36 +0200

    Initial commit

---
+++ components/header.tsx
@@ -0,0 +1,11 @@
+export const Header = () => {
+  return (
+    <header className="bg-gradient-to-b from-blue-700 to-blue-500 px-4 py-8 lg:px-14 pb-36">
+      <div className="max-w-screen-2xl mx-auto">
+        <div className="">
+          <div className=""></div>
+        </div>
+      </div>
+    </header>
+  );
+};
